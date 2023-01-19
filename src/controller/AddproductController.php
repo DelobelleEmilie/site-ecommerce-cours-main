26819a9bd45483941056520ef78f8ea4cbb5ee9a
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-01-19 09:59:14 +0100

    Chapitre 14

--- src/controller/AddproductController.php
+++ src/controller/AddproductController.php
@@ -3,13 +3,22 @@
 include_once '../src/model/ProductModel.php';
 include_once '../src/model/CategoryModel.php';
 
-$categories = getAllCategories();
+
 
 
 #Si on saisis le formulaire $_POST contient les données ducoup les produit s'enregistre et raffiche le formulaire vide
 function addProductController($twig, $db)
 {
   include_once '../src/model/ProductModel.php';
+    $categories = getAllCategories();
+    #variable de type tableau que nous allons utiliser pour paramétrer l’envoi de fichier.
+    $uploads = [
+        #clé «extensions» permettant de lister l’ensemble des extensions des fichiers qui sont autoriséesà être envoyées
+        'extensions' => ['png', 'jpg'],
+        'path' => 'uploads/','state' => false
+    ];
+    #défaut défini à «null»
+    $file_name = null;
   #Une variable «$form» est créée et définie en tant que tableau vide «[]»
   $form = [];
   if (isset($_POST['btnAddProduct'])) {
@@ -20,31 +29,68 @@ function addProductController($twig, $db)
       $price = 0.00;
     }
     $category = htmlspecialchars($_POST['productCategory']);
+    ##vérifie qu’une image est bien envoyée
+      if (isset($_FILES["productImage"]))
+      {
+          if (!empty($_FILES["productImage"]['name'])) {$file_upload = explode(".", $_FILES["productImage"]['name']);
+              // Vérification de l'extension
+              # permet de vérifier la présence d’une valeur dans un tableau.
+              if (in_array($file_upload[count($file_upload) -1],
+                  $uploads['extensions'])) {
+                  // Nettoyage des accents
+                  $file_name = strtr($file_upload[0],
+                      'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+                  // Nettoyage des caractères spéciaux
+                  $file_name = preg_replace('/([^.a-z0-9]+)/i', '_', $file_name);
+                  # permet de couper la chaine de caractères à chaque fois qu’un point sera présent.
+                  $file_name = $file_name . '.' .
+                      $file_upload[count($file_upload) -1];
+                  $file_path = $uploads['path'] . $file_name
+                      #’instruction de la condition s’attarde à vérifier qu’un fichier ayant le même nom n’existe pas.
+                  #Si aucun fichier du même nom n’est trouvé, l’envoi de fichier est autorisé à continuer
+                  #Si toutes les conditionssont vérifiées,le déplacement du fichier vers ledossier «uploads» situé dans le dossier «public» est autorisé. Le déplacement s’effectue
 
-      if (empty($label) || empty($description) || empty($category)) {
-          #la variable «$form» se constitue d’une clé «state» pour définir l’état du formulaire.
-          #Le tableau est aussi constitué d’une clé «message» danslaquelle nous renseignerons un message personnalisé pour informer l’utilisateur
-          $form = [
-              'state' => 'danger',
-              'message' => 'Votre produit n\'a pas été ajouté car les champs obligatoires n\'ont pas été remplis !'
-          ];
-          #la variable «$form» est complétéeavec des informations que nous pourrons réutiliser dans la vue Twig.
-          echo $twig->render('form_addproduct.html.twig', [
-              #la variable «$form» est passée à lavue Twig sous le nom «form»
-              'form' => $form,
-              'page'=>"?page=addProduct"
-          ]);
+                  if (!file_exists($file_path)) {
+                      // Déplacement du fichier
+# l’autorisation est effectuée en passant l’état de la variable «uploads» à vrai
+                      move_uploaded_file($_FILES['productImage']['tmp_name'], $file_path);
+                      $uploads['state'] = true;} else {$msg = "Une image avec ce nom existe déjà !";
+                  }} else
+                  {$msg = "L'extension du fichier n'est pas acceptée!";
+                  }} else
+                  {$msg = "Veuillez choisir un fichier !";
+          }}
+                  }
+              }
+              if (empty($label) || empty($description) || empty($category)) {
+                {
+                  #la variable «$form» se constitue d’une clé «state» pour définir l’état du formulaire.
+                  #Le tableau est aussi constitué d’une clé «message» danslaquelle nous renseignerons un message personnalisé pour informer l’utilisateur
+                  $form = [
+                      'state' => 'danger',
+                      'message' => [$msg]
+ 
+                  ];
+                  saveProduct($db, $label, $description, $price, $category, $file_name);
+
+                  #la variable «$form» est complétéeavec des informations que nous pourrons réutiliser dans la vue Twig.
+                  echo $twig->render('form_addproduct.html.twig', [
+                      #la variable «$form» est passée à la vue Twig sous le nom «form»
+                      'form' => $form,
+                      'page'=>"?page=addProduct"
+                  ]);
 #sinon le produit est sauvergardée
-      } else {
-          [
-              saveProduct($db, $label, $description, $price, $category)
-          ];
-          #la variable «$form» est complétéeavec des informations que nous pourrons réutiliser dans la vue Twig.
+              }
+                  else {
+                      $form= [
+                            if ($uploads['state']){
+                                $form=[
+                                'state' => "success",
+                            'message' => "Votre produit a bien était ajoute"
+    ];
           echo $twig->render('form_addproduct.html.twig', [
-              'form' => $form,
-          'page'=>"?page=addProduct"
+              'form' => $form
           ]);
       }
-
-    }
+                            }
 }
--- src/model/ProductModel.php
+++ src/model/ProductModel.php
@@ -7,7 +7,7 @@ function getallProduct($db)
 /*query = db qui prepare la selection de l'id, le label,...
 qui provienne de la classse shop ou la cle primare est id et est défini par where*/
 {
-    $query = $db->prepare("SELECT id, label, `description`, price, idCategory FROM shop_product");
+    $query = $db->prepare("SELECT id, label, `description`, price, idCategory,image  FROM shop_product");
 #On récupères tout les résultats de la requete et stocker le resultat dans la variable product
     $query->execute();
 /* On récupères touts résultats de la requete et stocker le resultat dans la variable product et retourner le resultat*/
@@ -20,13 +20,14 @@ function saveProduct($db, $label, $description, $price, $category)
     #la fonction query combine à la fois l'exécution de la requête et la mise en mémoire tampon du jeu de résultats
 /*query = db qui prepare l'insersertion des données dans la base shop_product*/
 # value = valeur de label, descr,...
-    $query = $db->prepare("INSERT INTO shop_product (label, 'description', price, idCategory)VALUE(:label, :descr, :price, :idCategory)");
+    $query = $db->prepare("INSERT INTO shop_product (label, 'description', price, idCategory,image)VALUE(:label, :descr, :price, :idCategory,:image )");
    #retourne le résultat et implanter des routes pour les données
     return $query->execute([
         'label' => $label,
         'descr' => $description,
         'price' => $price,
         'category' => $category,
+        'image' => $image
     ]);
 
     function updateOneProduct($db,$id,$label,$description,$price,$category)
