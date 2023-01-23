8866f6b2e1f701f4d77da8b18dd11d1271f5d95f
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-01-23 10:45:05 +0100

    Chap 14

--- src/service/upload.php
+++ src/service/upload.php
@@ -4,14 +4,13 @@ function upload($file): string | null
 {
     $uploads = [
         'extensions' => ['png', 'jpg'],
-        'path' => 'uploads/','state' => false]
-    ;
+        'path' => 'uploads/',
+        'state' => false
+    ];
 
     $file_name = null;
-
-    if (isset($file)) {
-        if (!empty($file['name'])) {
-            var_dump($file['name']);
+    if (isset($_FILES["productImage"])) {
+        if (!empty($_FILES["productImage"]['name'])) {
             $file_upload = explode(".", $_FILES["productImage"]['name']);
             // Vérification de l'extension
             # permet de vérifier la présence d’une valeur dans un tableau.
@@ -44,8 +43,40 @@ function upload($file): string | null
         } else {
             $msg = "Veuillez choisir un fichier !";
         }
+
+
+        if (isset($_POST['submit'])) {
+            $file = $_FILES['file'];
+
+            $fileName = $_FILES['file']['name'];
+            $fileTmpName = $_FILES['file']['tmp_name'];
+            $fileSize = $_FILES['file']['size'];
+            $fileError = $_FILES['file']['error'];
+            $fileType = $_FILES['file']['type'];
+
+            $fileExt = explode('.', $fileName);
+            $fileActualExt = strtolower(end($fileExt));
+
+            $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+            if (in_array($fileActualExt, $allowed)) {
+                if ($fileError === 0) {
+                    if ($fileSize < 1000000) {
+                        $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+                        $fileDestination = 'uploads/' . $fileNameNew;
+                        move_uploaded_file($fileTmpName, $fileDestination);
+                        header("Location: index.php?uploadsuccess");
+                    } else {
+                        echo "Your file is too big!";
+                    }
+                } else {
+                    echo "There was an error uploading your file!";
+                }
+            } else {
+                echo "You cannot upload files of this type!";
+            }
+        }
+
+
     }
-
-    return $file_name;
 }
-
