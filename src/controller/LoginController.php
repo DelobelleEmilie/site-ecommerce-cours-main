64f1f8629f7fc7af3dfa11b90632ba4656a64931
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-01-24 17:15:34 +0100

    Chap 17

--- src/controller/LoginController.php
+++ src/controller/LoginController.php
@@ -1,1 +1,11 @@
 <?php
+
+function LoginController($twig, $db)
+{
+    $params = [
+        'mail' => $object['mail'],
+        'mot_passe' => $object['mot_passe'],
+    ];
+    $_SESSION['test'] = "une valeur stockées";
+}
+
--- src/controller/RegisterController.php
+++ src/controller/RegisterController.php
@@ -2,5 +2,14 @@
 
 function RegisterController($twig, $db)
 {
+    $params = [
+        'mail' => $object['mail'],
+        'date_naissance' => $object['date_naissance'],
+        'prenom' => $object['prénom'],
+        'nom' => $object['nom'],
+        'mot_passe' => $object['mot_passe'],
+        'telephone' => $object['télephone']
+    ];
     $_SESSION['test'] = "une valeur stockées";
 }
+
