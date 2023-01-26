74fa06e69f99eee711cc8ea26237c748d9088a2b
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-01-26 10:41:59 +0100

    Chap 17

--- src/controller/RegisterController.php
+++ src/controller/RegisterController.php
@@ -1,15 +1,36 @@
 <?php
+function registerController($twig, $db)
 
-function RegisterController($twig, $db)
 {
-    $params = [
-        'mail' => $object['mail'],
-        'date_naissance' => $object['date_naissance'],
-        'prenom' => $object['prénom'],
-        'nom' => $object['nom'],
-        'mot_passe' => $object['mot_passe'],
-        'telephone' => $object['télephone']
-    ];
-    $_SESSION['test'] = "une valeur stockées";
+    include_once '../src/model/UserModel.php';
+}
+$form = [];
+if (isset($_POST['btnPostRegister']))
+{
+    $email = $_POST['userEmail'];
+}
+    $password = $_POST['userPassword'];
+    $passwordConfirm = $_POST['userPasswordConfirm'];
+    $lastname = $_POST['userLastname'];
+    $firstname = $_POST['userFirstname'];
+    if (count(getOneUserCredentials($db, $email)) === 0)
+    {
+        if ($password === $passwordConfirm)
+        {saveUser($db, $email, password_hash($password, PASSWORD_DEFAULT), $lastname, $firstname, 1);
+            $form = ['state' => 'success','message' => "Vous êtes maintenant inscrit au site"];
+        }
+        else {
+            $form = [
+                'state' => 'danger',
+                'message' => "Les deux mots de passe ne correspondent pas !"];
+        }
 }
 
+else {$form = [
+    'state' => 'danger',
+    'message' => "Un compte avec cette adresse mail existe déjà !"];
+
+}
+echo $twig->render('register.html.twig', ['form' => $form]);
+
+}
