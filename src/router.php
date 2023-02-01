3244cce0a3e004009adaa046dda14e1529bbcd1a
Rémy Vissyrias <remy.vissyrias@gmail.com>
2023-02-01 13:44:48 +0100

    Fix router

--- src/router.php
+++ src/router.php
@@ -3,42 +3,37 @@
 
 function initRouter($routes)
 {
-          //Si la variable page existe donc dans l'url on a mis ?page=truc alors $page devient ?page
-        if (isset($_GET['page']))
-        {
-                $page = $_GET['page'];
-        } else
-        {
-                $page = 'home';
+    //Si la variable page existe donc dans l'url on a mis ?page=truc alors $page devient ?page
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    } else {
+        $page = 'home';
+    }
+
+
+    //Si page est dans le tableau des routes alors $route devient la route (homeController)
+    if (isset($routes[$page])) {
+        $route = $routes[$page];
+
+    //Si la page n'est pas dans le tableau des routes alors $route afficher la page d'erreur
+    } else {
+        $route = $routes['error'];
+    }
+
+    $arguments = explode(":", $route);
+    $controller = ucfirst($arguments[0]);
+    $access =  count($arguments) > 1 ? explode(",", $arguments[1]) : [2]; // admin seulement par défaut
+
+    if (!in_array('0', $access)) {
+        if (!isset($_SESSION['auth']) || !in_array($_SESSION['auth']['role'], $access)) {
+            $controller = "HomeController";
         }
+    }
 
+    //Require le fichier controller - ex : controller/HomeController.php
+    require_once 'controller/' . $controller . '.php';
 
-
-//Si page est dans le tableau des routes alors $route devient la route (homeController)
-        if (isset($routes[$page])) 
-        {
-                $route = $routes[$page];
-                
-    //Si la page n'est pas dans le tableau des routes alors $route afficher la page d'erreur 
-        } else 
-        {
-                $route = $routes['error'];
-        }
-        $controller = ucfirst($route);
-        $routeParameters = explode('.',$route);
-        $controller =ucfirst($routeParameters[0]);
-        $access = $routeParameters[1] ?? 0;
-        if ($access !=0)
-        {
-            if (!isset($_SESSION['auth']) ||$_SESSION['auth']['role']< $access)
-                {
-    $controller ="HomeController";
-                }
-        }
-    //Require le fichier controller HomeController.php
-        require_once 'controller/' .$controller.'.php';
-
-        return $controller;
+    return $controller;
 }
 
 
