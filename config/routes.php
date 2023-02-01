74177e39c824febfbe180fbb6434b3c6cce4cd82
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-02-01 09:31:55 +0100

    routes de mot de passe

--- config/routes.php
+++ config/routes.php
@@ -3,37 +3,37 @@
 $routes =
     [
         // Shop
-        'home' => "homeController",                         // Liste des produits mis en avant
-        'showProductByCategory' => 'showProductByCategoryController',
-        'showProduct' => "showProductController",           // Fiche d'un produit
-        'about' => "aboutController",                       // Page à propos
-        'contact' => "contactController",                   // Page de contact
-        'error' => "errorController",                       // Page d'erreur
-        'mentionslegales' => "mentionslegalesController",   // Page des mentions légales
+        'home' => "homeController:0,1,2",                // Liste des produits mis en avant
+        'showProductByCategory' => 'showProductByCategoryController:0,1,2',
+        'showProduct' => "showProductController:0,1,2",    // Fiche d'un produit
+        'about' => "aboutController;0,1,2",                 // Page à propos
+        'contact' => "contactController:0,1,2",            // Page de contact
+        'error' => "errorController:0,1,2",                // Page d'erreur
+        'mentionslegales' => "mentionslegalesController:0,1,2",   //pages mentions légales
 
         // Authentication
-        'login' => "loginController",                       // Formulaire de connexion
-        'register' => "registerController",                 // Formulaire d'inscription
-        'logout' => "logoutController",                     // Déconnexion
+        'login' => "loginController:0,1,2",                       // Formulaire de connexion
+        'register' => "registerController:0,1,2",                 // Formulaire d'inscription
+        'logout' => "logoutController:0,1,2",                     // Déconnexion
 
         // Admin - Product
-        'adminProductList' => 'AdminProductListController',
-        'adminProductShow' => 'AdminProductShowController',
-        'adminProductEdit' => 'AdminProductEditController',
-        'adminProductAdd' => 'AdminProductAddController',
-        'adminProductDelete' => 'AdminProductDeleteController',
+        'adminProductList' => 'AdminProductListController:2',
+        'adminProductShow' => 'AdminProductShowController:2',
+        'adminProductEdit' => 'AdminProductEditController:2',
+        'adminProductAdd' => 'AdminProductAddController:2',
+        'adminProductDelete' => 'AdminProductDeleteController:2',
 
         // Admin - Category
-        'adminCategoryList' => 'AdminCategoryListController',
-        'adminCategoryEdit' => 'AdminCategoryEditController',
-        'adminCategoryAdd' => 'AdminCategoryAddController',
-        'adminCategoryDelete' => 'AdminCategoryDeleteController',
+        'adminCategoryList' => 'AdminCategoryListController:2',
+        'adminCategoryEdit' => 'AdminCategoryEditController:2',
+        'adminCategoryAdd' => 'AdminCategoryAddController:2',
+        'adminCategoryDelete' => 'AdminCategoryDeleteController:2',
         
         // Admin - User
-        'adminUserList' => 'AdminUserListController',
-        'adminUserShow' => 'AdminUserShowController',
-        'adminUserEdit' => 'AdminUserEditController',
-        'adminUserAdd' => 'AdminUserAddController',
-        'adminUserDelete' => 'AdminUserDeleteController'
+        'adminUserList' => 'AdminUserListController:2',
+        'adminUserShow' => 'AdminUserShowController:2',
+        'adminUserEdit' => 'AdminUserEditController:2',
+        'adminUserAdd' => 'AdminUserAddController:2',
+        'adminUserDelete' => 'AdminUserDeleteController:2'
     ];
 
