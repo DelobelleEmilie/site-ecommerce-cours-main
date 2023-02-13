36cf011e7f0d5c2412cb6dc27053017d944dbedc
remy.vissyrias <remy.vissyrias@te61.fr>
2023-02-13 14:03:52 +0100

    Register user

--- src/controller/RegisterController.php
+++ src/controller/RegisterController.php
@@ -44,7 +44,7 @@ function registerController($twig, $db)
         if (isset($email) && strlen($email) > 0 && isset($firstname) && isset($lastname)) {
             if (count(getOneUserCredentials($db, $email)) === 0) {
                 if ($password === $passwordConfirm) {
-                    saveUser($db, $email, $password, $lastname, $firstname, 1);
+                    saveUser($db, $email, $password, $lastname, $firstname, 1, true);
                     $form['state'] = 'success';
                     $form['message'] = 'Vous êtes maintenant inscrit au site !';
 
